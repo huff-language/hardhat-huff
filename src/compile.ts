b74dfeac014117a79340e447ec159813751cb3f7
JetDeveloping <jetjadeja@gmail.com>
2021-12-29 23:07:44 -0800

    Added basic compiler functionality

---
+++ src/compile.ts
@@ -0,0 +1,8 @@
+import { HuffConfig } from "./types";
+import { Artifacts, ProjectPathsConfig } from "hardhat/types";
+
+export const compile = (
+  config: HuffConfig,
+  paths: ProjectPathsConfig,
+  artifacts: Artifacts
+) => {};
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,23 @@ import { TASK_COMPILE_HUFF } from "./task-names";
 
 import { TASK_COMPILE_GET_COMPILATION_TASKS } from "hardhat/builtin-tasks/task-names";
 import { extendConfig, subtask } from "hardhat/internal/core/config/config-env";
+import { compile } from "./compile";
+
+extendConfig((config) => {
+  const defaultConfig = { version: "latest" };
+  config.huff = { ...defaultConfig, ...config.huff };
+});
+
+subtask(
+  TASK_COMPILE_GET_COMPILATION_TASKS,
+  async (_, __, runSuper): Promise<string[]> => {
+    const otherTasks = await runSuper();
+    return [...otherTasks, TASK_COMPILE_HUFF];
+  }
+);
+
+subtask(TASK_COMPILE_HUFF, async (_, { config, artifacts }) => {
+  // This plugin is experimental, so this task isn't split into multiple
+  // subtasks yet.
+  await compile(config.huff, config.paths, artifacts);
+});
