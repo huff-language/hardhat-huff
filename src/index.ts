d57a44ed4efce53a9a90965cad1d35cc0a687659
JetDeveloping <jetjadeja@gmail.com>
2021-12-29 22:54:54 -0800

    Add base index file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "./type-extensions";
+import { TASK_COMPILE_HUFF } from "./task-names";
+
+import { TASK_COMPILE_GET_COMPILATION_TASKS } from "hardhat/builtin-tasks/task-names";
+import { extendConfig, subtask } from "hardhat/internal/core/config/config-env";
