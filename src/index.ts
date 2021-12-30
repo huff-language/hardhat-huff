8beb0f87f5dcd3d51c46b2212244c028fcb4d9e2
JetDeveloping <jetjadeja@gmail.com>
2021-12-29 19:54:41 -0800

    Changes

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,25 @@
-import { extendConfig, extendEnvironment } from "hardhat/config";
-import { lazyObject } from "hardhat/plugins";
-import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
-import path from "path";
+import { TASK_COMPILE_GET_COMPILATION_TASKS } from 'hardhat/builtin-tasks/task-names'
+import { extendConfig, subtask } from 'hardhat/internal/core/config/config-env'
+import { any } from 'hardhat/internal/core/params/argumentTypes'
 
-import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
-// This import is needed to let the TypeScript compiler know that it should include your type
-// extensions in your npm package's types file.
-import "./type-extensions";
+import { TASK_COMPILE_HUFF } from './task-names'
+import './type-extensions'
 
-extendConfig(
-  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
-    // We apply our default config here. Any other kind of config resolution
-    // or normalization should be placed here.
-    //
-    // `config` is the resolved config, which will be used during runtime and
-    // you should modify.
-    // `userConfig` is the config as provided by the user. You should not modify
-    // it.
-    //
-    // If you extended the `HardhatConfig` type, you need to make sure that
-    // executing this function ensures that the `config` object is in a valid
-    // state for its type, including its extensions. For example, you may
-    // need to apply a default value, like in this example.
-    const userPath = userConfig.paths?.newPath;
+extendConfig((config: any) => {
+  const defaultConfig = { version: 'latest' }
+  config.huff = { ...defaultConfig, ...config.huff }
+})
 
-    let newPath: string;
-    if (userPath === undefined) {
-      newPath = path.join(config.paths.root, "newPath");
-    } else {
-      if (path.isAbsolute(userPath)) {
-        newPath = userPath;
-      } else {
-        // We resolve relative paths starting from the project's root.
-        // Please keep this convention to avoid confusion.
-        newPath = path.normalize(path.join(config.paths.root, userPath));
-      }
-    }
-
-    config.paths.newPath = newPath;
+subtask(
+  TASK_COMPILE_GET_COMPILATION_TASKS,
+  async (_, __, runSuper): Promise<string[]> => {
+    const otherTasks = await runSuper()
+    return [...otherTasks, TASK_COMPILE_HUFF]
   }
-);
+)
 
-extendEnvironment((hre) => {
-  // We add a field to the Hardhat Runtime Environment here.
-  // We use lazyObject to avoid initializing things until they are actually
-  // needed.
-  hre.example = lazyObject(() => new ExampleHardhatRuntimeEnvironmentField());
-});
+subtask(TASK_COMPILE_HUFF, async (_, { config, artifacts }) => {
+  const configuration: any = config
+  const { compileHuff } = await import('./compilation')
+  await compileHuff(configuration.huff, config.paths, artifacts)
+})
